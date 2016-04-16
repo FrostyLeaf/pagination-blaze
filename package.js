04fb97bf9a052eb4b3a5bffaf76d276204c9c70d
FrostyLeaf <joc98@163.com>
2016-04-17 01:00:06 +0800

    change to ver 0.0.2

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     "name": "leaf4monkey:pagination-blaze",
     "summary": "add blaze supporting to leaf4monkey:pagination.",
-    "version": "0.0.1",
+    "version": "0.0.2",
     "git": "https://github.com/FrostyLeaf/pagination-blaze.git",
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
     api.versionsFrom("METEOR@1.2.1");
-    api.use(['ecmascript', 'leaf4monkey:pagination@0.0.1']);
+    api.use(['ecmascript', 'leaf4monkey:pagination@0.0.2']);
 
     api.use([
         "meteor-base",
